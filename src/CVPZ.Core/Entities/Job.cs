5f5a416d4a0af8d660e77a8798433df0c5ca3265
carter.barnes <carter.barnes@cremadevelopment.com>
2020-07-01 21:20:07 -0700

    Add Job and link to Journal

---
+++ src/CVPZ.Core/Entities/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CVPZ.Core.Entities
+{
+    public class Job
+    {
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset StartDate { get; set; }
+        public DateTimeOffset? EndDate { get; set; }
+        public IEnumerable<JournalEntry> Journal { get; set; }
+    }
+}
