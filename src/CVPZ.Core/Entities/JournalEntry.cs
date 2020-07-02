e23daf565250a1b97df23d81f8c314e7fa4e4ebf
carter.barnes <carter.barnes@cremadevelopment.com>
2020-07-01 20:57:24 -0700

    Add application service for structure

---
+++ src/CVPZ.Core/Entities/JournalEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CVPZ.Core.Entities
+{
+    public class JournalEntry
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public List<string> Technologies { get; set; }
+    }
+}
---
+++ src/CVPZ.Infrastructure/Data/CVPZContext.cs
@@ -0,0 +1,23 @@
+﻿using CVPZ.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CVPZ.Infrastructure.Data
+{
+    public class CVPZContext : DbContext
+    {
+        public CVPZContext(DbContextOptions<CVPZContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JournalEntry>()
+                .Property(x => x.Description)
+                .IsRequired();
+        }
+
+        public DbSet<JournalEntry> JournalEntries { get; set; }
+    }
+}
