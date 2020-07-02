761abc5f298232e9ac11e0562a84c879bee2912f
carter.barnes <carter.barnes@cremadevelopment.com>
2020-07-01 21:42:15 -0700

    Update Job to have an add Journal action

--- src/CVPZ.Core/Entities/Job.cs
+++ src/CVPZ.Core/Entities/Job.cs
@@ -5,11 +5,24 @@ namespace CVPZ.Core.Entities
 {
     public class Job
     {
+        private List<JournalEntry> _journalEntries;
+
+        public Job ()
+        {
+            _journalEntries = new List<JournalEntry>();
+        }
+
         public Guid Id { get; set; }
         public string CompanyName { get; set; }
         public string Title { get; set; }
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset? EndDate { get; set; }
-        public IEnumerable<JournalEntry> Journal { get; set; }
+
+        public IEnumerable<JournalEntry> Journal { get; private set; }
+
+        public void AddJournalEntry (JournalEntry entry)
+        {
+            _journalEntries.Add(entry);
+        }
     }
 }
