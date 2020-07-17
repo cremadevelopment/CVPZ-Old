0ab0a39bdf2e5256195f147b4f7f5b8d4bf69a08
carter.barnes <carter.barnes@cremadevelopment.com>
2020-07-17 07:34:04 -0700

    Add HttpClientModule

--- src/CVPZ/ClientApp/src/app/app.module.ts
+++ src/CVPZ/ClientApp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -24,6 +25,7 @@ import { AboutComponent } from './about/about.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
