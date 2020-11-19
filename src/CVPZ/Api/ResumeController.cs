1c96fc1c5015b4011eae7e67333fb55ce31dc941
Carter Barnes <carter.barnes@cremadevelopment.com>
2020-11-18 20:26:37 -0800

    Fake controller and request names

---
+++ src/CVPZ/Api/ResumeController.cs
@@ -0,0 +1,32 @@
+﻿using CVPZ.Application.Resume;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace CVPZ.Api
+{
+    public class ResumeController : BaseApiController
+    {
+        private readonly ILogger<ResumeController> _logger;
+
+        public ResumeController(ILogger<ResumeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Create")]
+        public async Task<CreateResumeResponse> Create(CreateResume createResume)
+        {
+            _logger.LogInformation("Recieved create resume request.");
+            throw new NotImplementedException("");
+        }
+
+        [HttpPost("Update/{resumeId}")]
+        public async Task<UpdateResumeResponse> Update([FromQuery]string resumeId, UpdateResume createResume)
+        {
+            _logger.LogInformation("Recieved create resume request.");
+            throw new NotImplementedException("");
+        }
+    }
+}
