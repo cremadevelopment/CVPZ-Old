f6d1c28fe4a244d4544a7ebdca828cff4125aba1
Carter Barnes <carter.barnes@cremadevelopment.com>
2020-11-18 20:24:39 -0800

    Add dbup migration project

---
+++ src/CVPZ.DbMigrations/Program.cs
@@ -0,0 +1,51 @@
+﻿using DbUp;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Reflection;
+
+namespace CVPZ.DbMigrations
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Migration starting. ");
+            var connectionString = GetConnectionString(args.Length > 0 ? args[0] : null);
+
+            EnsureDatabase.For.SqlDatabase(connectionString); //Creates database if not exist
+
+            var upgradeEngineBuilder = DeployChanges.To
+                            .SqlDatabase(connectionString, null)
+                            .JournalToSqlTable("versions", "CVPZ_SchemaVersion")
+                            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                            .WithTransaction()
+                            .LogToConsole();
+
+            var upgrader = upgradeEngineBuilder.Build();
+            if (upgrader.IsUpgradeRequired())
+            {
+                var results = upgrader.PerformUpgrade();
+                if (results.Successful)
+                {
+                    Console.WriteLine("Database upgrade success");
+                }
+            } else
+            {
+                Console.WriteLine("No upgrade is required");
+            }
+        }
+
+        private static string GetConnectionString(string connString)
+        {
+
+            if (!string.IsNullOrWhiteSpace(connString)) return connString;
+
+            IConfiguration config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json",
+                        true,
+                        true).Build();
+
+            return config.GetConnectionString("EventStoreDatabase");
+        }
+    }
+}
