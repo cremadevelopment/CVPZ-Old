048cf6ba6dad10300e2864dced3028bfed7d76bb
Carter Barnes <carter.barnes@cremadevelopment.com>
2020-11-23 15:11:34 -0800

    Add message request logger

---
+++ src/CVPZ.Application/Common/Behaviors/RequestLogger.cs
@@ -0,0 +1,26 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CVPZ.Application.Common.Behaviors
+{
+    public class RequestLogger<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger _logger;
+
+        public RequestLogger(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var name = typeof(TRequest).Name;
+
+            _logger.LogInformation("CVPZ Request: {Name} {@Request}", name, request);
+
+            return Task.CompletedTask;
+        }
+    }
+}
