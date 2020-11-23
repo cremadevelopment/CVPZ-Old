d6d095d12a2b6e648b4ff64b66a27d35efa56ccf
Carter Barnes <carter.barnes@cremadevelopment.com>
2020-11-23 15:19:58 -0800

    Add performance behavior to report requests over 500 ms.

---
+++ src/CVPZ.Application/Common/Behaviors/RequestPerformanceBehavior.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CVPZ.Application.Common.Behaviors
+{
+    public class RequestPerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+
+        public RequestPerformanceBehavior(ILogger<TRequest> logger)
+        {
+            _timer = new Stopwatch();
+
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _timer.Start();
+
+            var response = await next();
+
+            _timer.Stop();
+
+            if (_timer.ElapsedMilliseconds > 500)
+            {
+                var name = typeof(TRequest).Name;
+
+                _logger.LogWarning("CVPZ Long Running Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}",
+                    name, _timer.ElapsedMilliseconds, request);
+            }
+
+            return response;
+        }
+    }
+}
