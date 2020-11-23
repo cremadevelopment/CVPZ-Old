f334f1ae864e8a9dbadd4309b30a733ee1f08e19
Carter Barnes <carter.barnes@cremadevelopment.com>
2020-11-23 15:08:49 -0800

    Update registration of services to be held within components.

---
+++ src/CVPZ.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,28 @@
+﻿using CVPZ.Core.Entities;
+using CVPZ.Core.Repository;
+using CVPZ.Infrastructure.Data;
+using CVPZ.Infrastructure.Factories;
+using CVPZ.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tactical.DDD.EventSourcing;
+
+namespace CVPZ.Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration Configuration)
+        {
+            services.AddDbContext<CVPZContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<ISqlConnectionFactory>(new SqlConnectionFactory(Configuration.GetConnectionString("EventStoreDatabase")));
+
+            services.AddTransient<IEventStore, EventStoreRepository>();
+            services.AddTransient<IRepository<JournalEntry>, JournalEntryRepository>();
+            services.AddTransient<IResumeRepository, ResumeRepository>();
+        }
+    }
+}
